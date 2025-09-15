1e33875b26bcc2b7297a2bee1522e4de601fb839
Chan-dra-k <chandra.k2024@vitstudent.ac.in>
2025-09-15 14:15:33 -0400

    Add Day 2: set hostname + mount /proc + ps in child

--- src/mini_container.c
+++ src/mini_container.c
@@ -4,20 +4,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/mount.h>
 
 #define STACK_SIZE (1024*1024)
 static char *child_stack;
 
 int child_fn(void *arg) {
-    printf("Hello from container! pid=%d\n", getpid());
-    sleep(2);
+    sethostname("miniOS", 6);
+    // try mounting proc (requires root)
+    mount("proc", "/proc", "proc", 0, "");
+    printf("Inside container: hostname -> ");
+    char h[100];
+    gethostname(h, sizeof(h));
+    printf("%s\n", h);
+    // show PID of self
+    printf("Inside container: pid=%d\n", getpid());
+    execlp("ps", "ps", "-ef", NULL);
     return 0;
 }
 
 int main() {
     child_stack = malloc(STACK_SIZE);
     if (!child_stack) { perror("malloc"); exit(1); }
-    int flags = CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD;
+    int flags = CLONE_NEWPID | CLONE_NEWUTS | CLONE_NEWNS | SIGCHLD;
     pid_t pid = clone(child_fn, child_stack + STACK_SIZE, flags, NULL);
     if (pid == -1) { perror("clone"); exit(1); }
     printf("[host] created child pid: %d\n", pid);
@@ -25,3 +34,5 @@ int main() {
     printf("[host] child finished\n");
     return 0;
 }
+
+
